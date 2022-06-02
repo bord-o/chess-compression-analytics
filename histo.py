80e6e828142ad96dad5f2ddfc822cb77dfc7f0f3
bord-o <brodylittle011@gmail.com>
2022-06-02 00:02:03 -0400

    histogram modification for initial rust testing

--- histo.py
+++ histo.py
@@ -21,19 +21,28 @@ with open("lichess_res.txt", "r") as lichess_res:
     plt.plot(index[:pltpoints], freq[:pltpoints])
 
 
-with open("./rust_test_stripped.csv", "r") as res:
+with open("./db_res.csv", "r") as res:
 
     x = res.read().split(",")
     x.pop(-1)
     y = []
     for item in x:
         y.append(int(item))
+    
+    elem_count = {}
+    for e in y:
+        if e in elem_count:
+            elem_count[e] += 1
+        else:
+            elem_count[e] = 1
+    for k,v in sorted(elem_count.items()):
+        print(f"{k}: {v}")
 
     y = np.array(y)
     # plt.hist(y,bins=np.arange(y.min(), y.max()+1))
     # plt.show()
 
-    print(list(map(int, x)))
+    # print(list(map(int, x)))
     print("Data skew: " + str(3 * (numpy.average(y) - numpy.median(y)) / numpy.std(y)))
     index, counts = numpy.unique(y, return_counts=True)
     print(len(index), len(counts))
@@ -68,5 +77,5 @@ with open("./modern35.csv", "r") as res:
         print(index[i], freq[i])
 
     plt.plot(index[:pltpoints], freq[:pltpoints])
-plt.show()
-plt.savefig("lc_vs_rust_vs__py_mod35.png")
+# plt.show()
+plt.savefig("rust_full_db_histo.png")
